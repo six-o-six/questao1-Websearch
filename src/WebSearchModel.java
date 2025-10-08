5f8bab2059a0e601f8d1bb5ded634ac06ff3ea0f
Jak <jakelynecavalcanti@gmail.com>
2025-10-07 23:16:10 -0300

    Initial commit: Implementar estrutura inicial do projeto
    Adicionar as classes WebSearchModel, Snooper e Main.java com o arquivo Hamlet.txt
    
    Este commit configura a arquitetura base em anexo no Google Classroom para simular o processamento de consultas, estabelecendo o Padrão Observer (WebSearchModel como Sujeito e Snooper como Observador)

---
+++ src/WebSearchModel.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Perform "web search" (from a  file), notify the interested observers of each query.
+ */
+public class WebSearchModel {
+    private final File sourceFile;
+    private final List<QueryObserver> observers = new ArrayList<>();
+
+    public interface QueryObserver {
+        void onQuery(String query);
+    }
+
+    public WebSearchModel(File sourceFile) {
+        this.sourceFile = sourceFile;
+    }
+
+    public void pretendToSearch() {
+        try (BufferedReader br = new BufferedReader(new FileReader(sourceFile))) {
+            while ( true) {
+                String line = br.readLine();
+                if (line == null) {
+                    break;
+                }
+                notifyAllObservers(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void addQueryObserver(QueryObserver queryObserver) {
+        observers.add(queryObserver);
+    }
+
+    private void notifyAllObservers(String line) {
+        for (QueryObserver obs : observers) {
+            obs.onQuery(line);
+        }
+    }
+}
